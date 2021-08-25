ff0921ea97874a1a833fd1bb3bcc52c4f9210895
exinmusic <exinmusic@gmail.com>
2021-08-25 11:02:08 -0700

    Update interface.py

--- software/current/interface.py
+++ software/current/interface.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     while True:
         if buttonB.value and not buttonA.value:  # just button A pressed
             oled.print_text('A!', '#FF0000')
+            print("A")
         if buttonA.value and not buttonB.value:  # just button B pressed
             oled.print_text('B!', '#00FF00')
+            print("B")
         if not buttonA.value and not buttonB.value:  # none pressed
             oled.print_text('WHAT DO YOU WANT FROM ME?', '#0000FF')
